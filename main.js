7675c358baf747cfc7254f832e114d660ddaef6e
trungluongdev <chitrungluong.cv@gmail.com>
2024-05-28 19:32:33 +0700

    push to review

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ async function getAllGames() {
     } catch (error) {
         console.log("error:", error);
     }
-
 }
 // getAllGames();
 async function loadAllGames() {
@@ -32,6 +31,9 @@ async function loadAllGames() {
             gameName.appendChild(p);
             Element.appendChild(img);
             Element.appendChild(gameName);
+            Element.onclick = function () {
+
+            };
             cover.appendChild(Element);
             loading.style.display = "none";
         })
@@ -51,12 +53,11 @@ async function getGamesByGenres() {
         return data;
     } catch (error) {
         console.log("error:", error)
-    }
+    };
 
-}
-getGamesByGenres();
+};
 let btn = document.querySelectorAll(".btn");
-async function gameByGenres() {
+async function gameByGenres(genres) {
     try {
         const data = await getAllGames();
         let cover = document.querySelector(".container-game");
@@ -76,7 +77,11 @@ async function gameByGenres() {
                 gameName.appendChild(p);
                 Element.appendChild(img);
                 Element.appendChild(gameName);
+                Element.onclick = function () {
+                    console.log(game.appid);
+                };
                 cover.appendChild(Element);
+
             }
         }
         )
@@ -88,7 +93,67 @@ async function gameByGenres() {
 
 btn.forEach(button => {
     button.addEventListener("click", (e) => {
-        genres = e.target.textContent.toLowercase();
+        genres = e.target.textContent.toLowerCase();
         gameByGenres(genres);
     });
 });
+
+
+// search function
+
+const typeGame = async () => {
+    const search = document.querySelector("#search-bar");
+    try {
+        let queryString = "";
+        if (search.value) {
+            queryString += `&steamspy_tags = ${search.value.toLowerCase()}`;
+            return queryString;
+            console.log(queryString);
+        };
+
+        const url = `${BASE_URL}/games?limit=30${queryString}`;
+
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log("data:", data);
+        return data;
+
+    } catch (err) {
+        console.log("error:", err.message);
+    }
+}
+typeGame()
+const loadGameTyped = async () => {
+    let loading = document.querySelector(".fui-loading-spinner-2");
+    try {
+        const data = await typeGame();
+
+        let cover = document.querySelector(".container-game");
+        cover.innerHTML = "";
+        data.data.forEach(game => {
+            let Element = document.createElement("a");
+            Element.classList.add("cover");
+            let img = document.createElement("img");
+            img.src = game.header_image;
+            img.classList.add("img")
+            let gameName = document.createElement("div");
+            gameName.classList.add("game-name");
+            let p = document.createElement("p");
+            p.textContent = game.name;
+            gameName.appendChild(p);
+            Element.appendChild(img);
+            Element.appendChild(gameName);
+            Element.onclick = function () {
+
+            };
+            cover.appendChild(Element);
+            loading.style.display = "none";
+        });
+    } catch (err) {
+        console.log("error:", err.message);
+    }
+
+};
+
+
+search.addEventListener("input", loadGameTyped);
